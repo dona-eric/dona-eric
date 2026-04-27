0ab7cd61cf3761c53e6f2b6d9f64e52e1d19bd0f
dona-eric <donaerickoulodji@gmail.com>
2026-04-27 11:58:35 +0100

    added the path masterclas en formations in sections

--- src/components/AllSections.jsx
+++ src/components/AllSections.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Hero from './Hero'
 import About from './About'
 import Services from './Services'
+import MasterclassSection from './Masterclass/MasterclassSection'
 import ContactForm from './ContactForm'
 export default function AllSections(){
   return (
     <div className="space-y-24">
       <Hero />
       <About />
-      <Services />
+      <MasterclassSection />
       <section id="contact" className="container mx-auto px-6">
         <ContactForm />
       </section>
